1a79b1287b89adde9151754e3b1310e868e825d9
Tilak_Shelke <147398031+tilakshelke08@users.noreply.github.com>
2024-01-12 23:29:48 +0530

    Add files via upload
    RefAndValType example

---
+++ Day4/RefAndValType2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndValType2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            class1 o = new class1();
+
+            o.i = 100;
+          //  Dosomething1(o);
+            Dosomething2(o);
+            Console.WriteLine(o.i);
+            Console.ReadLine();
+        }
+        static void Dosomething1(class1 obj)
+        {
+
+            obj.i = 200;
+        }
+        static void Dosomething2(class1 obj)
+        {
+            obj= new class1();
+            obj.i = 200;
+        }
+        public class class1
+        {
+            public int i;
+        }
+    }
+}
+
